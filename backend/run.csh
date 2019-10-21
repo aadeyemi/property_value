8caf2c1e39ae6980a65cb3e18a09b45b4a9ceccc
aadeyemi <mailyemi@gmail.com>
2019-10-20 20:06:39 -0400

    backend api test successful

--- backend/run.csh
+++ backend/run.csh
@@ -3,3 +3,6 @@ cd property_value_proj/
 python manage.py startapp property_value_app 
 pip install django-cors-headers
 pip install psycopg2
+
+create-react-app property_value_app
+git ls-tree --full-tree -r --name-only HEAD
---
+++ frontend/property_value_app/src/app.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function testApi() {
+  const api = "http://127.0.0.1:8000/api/v0/real-acct/texas/harris/1000/";
+
+  function headers(type, payload) {
+    const obj = {
+      method: type,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Token ${""}`,
+        "Content-Type": "application/json"
+      }
+    };
+
+    if (type === "POST") obj.body = JSON.stringify(payload);
+
+    return obj;
+  }
+
+  function handleErrors(response) {
+    if (!response.ok) {
+      throw Error(`Request rejected with status ${response.statusText}`);
+    }
+    return response;
+  }
+
+  function callback(data) {
+    return console.log(data);
+  }
+
+  fetch(api, headers("GET", {}))
+    .then(response => handleErrors(response))
+    .then(response => response.json())
+    .then(responseJson => callback(responseJson))
+    .catch(error => console.log(api, error));
+}
+
+function App() {
+  testApi();
+  return <div className="App" />;
+}
+
+export default App;
